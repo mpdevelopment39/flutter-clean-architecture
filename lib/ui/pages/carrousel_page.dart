ce9a4cd4d857123f2867079273465e50787d1859
Miguel Pérez Riesgo <mperez@opendit.com>
2024-04-19 22:50:40 +0200

    style: changes on carrousel ui

--- lib/ui/pages/carrousel_page.dart
+++ lib/ui/pages/carrousel_page.dart
@@ -6,12 +6,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:sizer/sizer.dart';
 import '../../app/theme.dart';
 
-
 //* Clase para gestionar la UI del Carrousel inicial
 class CarrouselPage extends StatefulWidget {
   static const name = 'carrousel-page';
   
-
   const CarrouselPage({super.key});
 
   @override
@@ -20,13 +18,22 @@ class CarrouselPage extends StatefulWidget {
 
 class _CarrouselPageState extends State<CarrouselPage> {
   LiquidController liquidController = LiquidController();
-  List<_CarrouselItem> data = [];
+  List<CarrouselItem> data = [];
   int pageIndex = 0;
   
   @override
   void initState() {
     super.initState();
     _startAnimation();
+  }
+
+  _startAnimation() async {
+    int startPage = liquidController.currentPage;
+    await Future.delayed(const Duration(seconds: 3));
+    int currentPage = liquidController.currentPage;
+    if (startPage == currentPage){
+      if(mounted) liquidController.animateToPage(page: pageIndex + 1);
+    }
   }
 
   Widget _buildDot(int index) {
@@ -51,74 +58,40 @@ class _CarrouselPageState extends State<CarrouselPage> {
     }
   }
 
-  _startAnimation() async {
-    int startPage = liquidController.currentPage;
-    await Future.delayed(const Duration(seconds: 3));
-    int currentPage = liquidController.currentPage;
-    if (startPage == currentPage){
-      if(mounted) liquidController.animateToPage(page: pageIndex + 1);
-    }
+  void fillData(BuildContext context) {
+    data = [
+      CarrouselItem(
+        AppTheme.kMoviesOrangeLight,
+        "assets/images/carrousel_1.svg",
+        AppLocalizations.of(context)!.carrousel_title1
+      ),
+      CarrouselItem(
+        AppTheme.kMoviesRedLight,
+        "assets/images/carrousel_2.svg",
+        AppLocalizations.of(context)!.carrousel_title2,
+      ),
+      CarrouselItem(
+        AppTheme.kMoviesPurpleLight,
+        "assets/images/carrousel_3.svg",
+        AppLocalizations.of(context)!.carrousel_title3,
+      ),
+      CarrouselItem(
+        AppTheme.kMoviesGreenLight,
+        "assets/images/carrousel_4.svg",
+        AppLocalizations.of(context)!.carrousel_title4,
+      ),
+    ];
   }
-
 
   @override
   Widget build(BuildContext context) {
-    data = [
-      _CarrouselItem(
-          AppTheme.kMoviesOrangeLight,
-          "assets/images/carrousel_1.svg",
-          AppLocalizations.of(context)!.carrousel_title1
-        ),
-        _CarrouselItem(
-          AppTheme.kMoviesRedLight,
-         "assets/images/carrousel_2.svg",
-          AppLocalizations.of(context)!.carrousel_title2,
-        ),
-        _CarrouselItem(
-          AppTheme.kMoviesPurpleLight,
-         "assets/images/carrousel_3.svg",
-          AppLocalizations.of(context)!.carrousel_title3,
-        ),
-        _CarrouselItem(
-          AppTheme.kMoviesGreenLight,
-         "assets/images/carrousel_4.svg",
-          AppLocalizations.of(context)!.carrousel_title4,
-        ),
-    ];
+    fillData(context);
     return Scaffold(
       body: LiquidSwipe.builder(
         itemCount: data.length,
         itemBuilder: (context, index) {
           return Stack(alignment: Alignment.center, children: [
-            Container(
-              color: data[index].backgroundColor,
-              child: SafeArea(
-                bottom: true,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(30,50,10,0),
-                      child: SizedBox(
-                        height: 130,
-                        child: Text(
-                          data[index].title,
-                          textAlign: TextAlign.left,
-                          style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontWeight: FontWeight.w400),
-                        ),
-                      )
-                    ),
-                    SizedBox(
-                      height: 40.h,
-                      width: 40.h,
-                      child: SvgPicture.asset(data[index].image),
-                    ),
-                    const Spacer(),
-                  ],
-                ),
-              ),
-            ),
+            _CarrouselView(item:data[index]),
             Positioned(
               left: 0,
               right: 0,
@@ -155,10 +128,49 @@ class _CarrouselPageState extends State<CarrouselPage> {
   }
 }
 
-class _CarrouselItem {
+class _CarrouselView extends StatelessWidget {
+  final CarrouselItem item;
+
+  const _CarrouselView({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: item.backgroundColor,
+      child: SafeArea(
+        bottom: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30,50,10,0),
+              child: SizedBox(
+                height: 130,
+                child: Text(
+                  item.title,
+                  textAlign: TextAlign.left,
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontWeight: FontWeight.w400),
+                ),
+              )
+            ),
+            SizedBox(
+              height: 40.h,
+              width: 40.h,
+              child: SvgPicture.asset(item.image),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CarrouselItem {
   final Color backgroundColor;
   final String image;
   final String title;
 
-  _CarrouselItem(this.backgroundColor, this.image, this.title);
+  CarrouselItem(this.backgroundColor, this.image, this.title);
 }
